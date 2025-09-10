fb17c8c3597e692fb3c7a873fbe73bceb7404df2
Andreas Nachtmann <virtualnonsense@mailbox.org>
2025-09-10 14:55:05 +0200

    ported some simple shortcuts over

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,3 +5,16 @@
 local map = vim.keymap.set
 
 map("n", "<A-CR>", vim.lsp.buf.code_action, { desc = "LSP: Code Action" })
+
+-- Search & replace
+map(
+  "n",
+  "<leader>rR",
+  [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+  { desc = "Replace word under cursor in file" }
+)
+
+-- Clipboard + delete behavior
+map("x", "<leader>p", [["_dP]], { desc = "Paste over selection" })
+map("n", "<leader>Y", [["+Y]], { desc = "Yank line to clipboard" })
+map({ "n", "v" }, "<leader>D", '"_d', { desc = "Delete without yank" })
