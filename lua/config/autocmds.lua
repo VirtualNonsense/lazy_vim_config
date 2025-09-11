26cc70fcb941420f058a387a1de46a6c41d99338
Andreas Nachtmann <andreas.nachtmann@mathema.de>
2025-09-11 15:46:01 +0200

    added a spinner autocommand to indicate that codecompanion is working in the background

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -5,4 +5,52 @@
 -- with `vim.api.nvim_create_autocmd`
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+-- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")        local group = vim.api.nvim_create_augroup("CodeCompanionFidgetHooks", { clear = true })
+--#region codecompanion status spinner
+local spinner = {
+  "🌑", -- new moon
+  "🌒", -- waxing crescent
+  "🌓", -- first quarter
+  "🌔", -- waxing gibbous
+  "🌕", -- full moon
+  "🌖", -- waning gibbous
+  "🌗", -- last quarter
+  "🌘", -- waning crescent
+}
+local group = vim.api.nvim_create_augroup("CodeCompanionFidgetHooks", { clear = true })
+vim.api.nvim_create_autocmd({ "User" }, {
+  pattern = "CodeCompanion*",
+  group = group,
+  callback = function(request)
+    if request.match == "CodeCompanionChatSubmitted" then
+      return
+    end
+
+    local msg
+
+    msg = "[CodeCompanion] " .. request.match:gsub("CodeCompanion", "")
+
+    vim.notify(msg, "info", {
+      timeout = 1000,
+      keep = function()
+        return not vim
+          .iter({ "Finished", "Opened", "Hidden", "Closed", "Cleared", "Created", "ContextChanged", "ChatDone" })
+          :fold(false, function(acc, cond)
+            return acc or vim.endswith(request.match, cond)
+          end)
+      end,
+      id = "code_companion_status",
+      title = "Code Companion Status",
+      opts = function(notif)
+        notif.icon = ""
+        if vim.endswith(request.match, "Started") then
+          ---@diagnostic disable-next-line: undefined-field
+          notif.icon = spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+        elseif vim.endswith(request.match, "Finished") then
+          notif.icon = " "
+        end
+      end,
+    })
+  end,
+})
+--#endregion
