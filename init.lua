2bbfa007270b51a54f6d6c40446210ca8a3be7bf
Andreas Nachtmann <virtualnonsense@mailbox.org>
2025-11-07 13:51:52 +0100

    basic overseer setup

--- init.lua
+++ init.lua
@@ -2,3 +2,6 @@
 require("config.lazy")
 require("config.lsp-config")
 require("config.jupynuim")
+require("overseer").setup({
+  templates = { "builtin", "user" },
+})
---
+++ lua/plugins/overseer.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "stevearc/overseer.nvim",
+    keys = {
+      { "<leader>ot", "<cmd>OverseerToggle<cr>", desc = "Tasks: Toggle" },
+      { "<leader>or", "<cmd>OverseerRun<cr>", desc = "Tasks: Run" },
+      { "<leader>ob", "<cmd>OverseerBuild<cr>", desc = "Tasks: Build picker" },
+      { "<leader>ox", "<cmd>OverseerQuickAction<cr>", desc = "Tasks: Actions" },
+    },
+  },
+}
