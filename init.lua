11f14f8e331aea6a67ee41ae81178730f950693e
Andreas Nachtmann <virtualnonsense@mailbox.org>
2026-03-10 12:58:10 +0100

    moved lsp config to plugins because the control seems to be more reliable that way

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-require("config.lsp-config")
 require("config.jupynuim")
 require("overseer").setup({
   templates = { "builtin", "user" },
---
+++ lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,68 @@
+local lspconfig = require("lspconfig")
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      tsq = {},
+      clangd = {},
+      cssls = {},
+      html = {},
+      jsonls = {},
+      pyright = {
+        settings = {
+          pyright = {
+            disableOrganizeImports = true,
+          },
+          python = {
+            analysis = {
+              typeCheckingMode = "basic",
+              autoImportCompletions = true,
+              autoSearchPaths = true,
+              useLibraryCodeForTypes = true,
+            },
+          },
+        },
+      },
+
+      ruff = {
+        cmd = { "ruff", "server" },
+        filetypes = { "python" },
+        root_dir = lspconfig.util.root_pattern("pyproject.toml", "ruff.toml", ".ruff.toml", ".git"),
+        init_options = {
+          settings = {
+            args = {},
+          },
+        },
+        on_attach = function(client)
+          client.server_capabilities.hoverProvider = false
+        end,
+      },
+      texlab = {
+        settings = {
+          texlab = {
+            auxDirectory = "build",
+            build = {
+              args = { "-pdf", "-shell-escape", "-interaction=nonstopmode", "-synctex=1", "%f" },
+              onSave = true,
+              forwardSearchAfter = true,
+            },
+            chktex = {
+              onOpenAndSave = true,
+            },
+            forwardSearch = {
+              executable = "zathura",
+              args = { "--synctex-forward", "%l:1:%f", "%p" },
+            },
+          },
+        },
+      },
+      tinymist = {
+        cmd = { "tinymist" },
+      },
+      tombi = {
+        filetypes = { "toml" },
+        root_markers = { "tombi.toml", "pyproject.toml", ".git", "Cargo.toml" },
+      },
+    },
+  },
+}
