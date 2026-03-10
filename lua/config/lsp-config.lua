eed62608921216730ee73d1cf8ba7bf25db0f7b2
Andreas Nachtmann <virtualnonsense@mailbox.org>
2026-03-10 12:03:46 +0100

    simplified config

--- lua/config/lsp-config.lua
+++ lua/config/lsp-config.lua
@@ -1,46 +1,7 @@
 local lspconfig = require("lspconfig")
 
--- Normalize: bool capabilities -> false, table capabilities -> nil (entfernen)
-local function disable_capability(client, key)
-  local v = client.server_capabilities[key]
-  if type(v) == "table" then
-    client.server_capabilities[key] = nil
-  else
-    client.server_capabilities[key] = false
-  end
-end
-
--- Whitelist-Filter: lässt nur die Keys in `whitelist` bestehen
-local function apply_capability_whitelist(client, whitelist)
-  -- build fast lookup
-  local allow = {}
-  for _, k in ipairs(whitelist or {}) do
-    allow[k] = true
-  end
-
-  -- Debug: vor dem Patch anzeigen
-  vim.notify(
-    vim.inspect(client.server_capabilities),
-    vim.log.levels.INFO,
-    { title = ("%s capabilities (before whitelist)"):format(client.name) }
-  )
-
-  -- Alles killen, was nicht explizit erlaubt ist
-  for key, _ in pairs(client.server_capabilities) do
-    if not allow[key] then
-      disable_capability(client, key)
-    end
-  end
-
-  -- Debug: nach dem Patch anzeigen
-  vim.notify(
-    vim.inspect(client.server_capabilities),
-    vim.log.levels.INFO,
-    { title = ("%s capabilities (after whitelist)"):format(client.name) }
-  )
-end
-
 local servers = {
+  tsq = {},
   clangd = {},
   cssls = {},
   html = {},
